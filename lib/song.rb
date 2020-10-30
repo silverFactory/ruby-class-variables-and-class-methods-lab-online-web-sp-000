ba4e44ba7efaddcbd962f81c774ed22cc618b7c8
Will Studabaker <will.studabaker@gmail.com>
2020-10-30 15:52:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@song_count = 0
+  @@count = 0
   @@artists = []
   @@genres = []
 
   def initialize(name, artist, genre)
-    @@song_count += 1
+    @@count += 1
     @name = name
     @artist = artist
     @@artists << name
